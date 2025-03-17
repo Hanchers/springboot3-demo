fb2ce36d182b2ca7f207368e90502b65eda7d8c3
hancher <hanchers@outlook.com>
2025-03-17 09:31:24 +0800

    ai demo

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 a demo for springboot3 feature
 
 项目文档: https://docs.spring.io/spring-boot/tutorial/first-application/index.html
+
+
+[spring ai](https://docs.spring.io/spring-ai/reference/index.html)
---
+++ hello-ai/src/main/java/com/hancher/springboot3/helloai/HelloAiApplication.java
@@ -0,0 +1,13 @@
+package com.hancher.springboot3.helloai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloAiApplication.class, args);
+	}
+
+}
